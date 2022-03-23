eb749c7d6b5fe48d9c8b53cc5753f7e15aeae6ff
tunedcoder <thetunedcoder@gmail.com>
2022-03-23 08:04:41 +0530

    first commit

---
+++ src/server/arenuo/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ArenuoConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'arenuo'
---
+++ src/server2/aruneo/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AruneoConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'aruneo'
